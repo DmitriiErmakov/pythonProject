d1789eaae4af9ff162dc38d65c77b7087be44ca1
ermak <ermakov111@mail.ru>
2023-06-08 09:23:12 +0300

    add test

--- test_google_search.py
+++ test_google_search.py
@@ -5,8 +5,8 @@ from selene import be, have
 
 @pytest.fixture
 def browser_open():
-    browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.window_width = 1920
     browser.open('https://google.com')
 
 
