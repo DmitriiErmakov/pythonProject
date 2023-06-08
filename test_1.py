015b4a86ffcc132aa9116b73c1491ff65952ff29
ermak <ermakov111@mail.ru>
2023-06-08 09:03:53 +0300

    add test

--- test_1.py
+++ test_1.py
@@ -5,13 +5,16 @@ from selene import be, have
 
 @pytest.fixture
 def browser_open():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.open('https://google.com')
-    browser.driver.maximize_window()
 
-def test_1(browser_open):
+
+def test_search_selene(browser_open):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_2(browser_open):
+
+def test_search_no_result(browser_open):
     browser.element('[name="q"]').should(be.blank).type('ldfjgljdfgjklfg').press_enter()
     browser.element('[id="topstuff"]').should(have.text('По запросу ldfjgljdfgjklfg ничего не найдено'))
